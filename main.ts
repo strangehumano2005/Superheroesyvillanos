d827ba39bc316ae4de502509f14f37bf2b928fd1
strangehumano2005 <112585795+strangehumano2005@users.noreply.github.com>
2022-10-17 07:40:52 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,34 @@
 // Determinar posiciones de personajes
 // 
-let CLARK_KENT = game.createSprite(0, 2)
-let LEX_LUTHOR = game.createSprite(randint(1, 4), randint(0, 4))
+let Spiderman = game.createSprite(0, 2)
+let Maléfica = game.createSprite(randint(1, 4), randint(0, 4))
 basic.forever(function () {
     while (input.buttonIsPressed(Button.AB)) {
-        CLARK_KENT.change(LedSpriteProperty.Y, 1)
+        Spiderman.change(LedSpriteProperty.Y, 1)
         basic.pause(200)
     }
     while (input.buttonIsPressed(Button.A)) {
-        CLARK_KENT.change(LedSpriteProperty.X, -1)
+        Spiderman.change(LedSpriteProperty.X, -1)
         basic.pause(200)
     }
     while (input.buttonIsPressed(Button.B)) {
-        CLARK_KENT.change(LedSpriteProperty.X, 1)
+        Spiderman.change(LedSpriteProperty.X, 1)
         basic.pause(200)
     }
     while (input.logoIsPressed()) {
-        CLARK_KENT.change(LedSpriteProperty.Y, -1)
+        Spiderman.change(LedSpriteProperty.Y, -1)
         basic.pause(200)
     }
-    if (CLARK_KENT.isTouching(LEX_LUTHOR)) {
-        CLARK_KENT.delete()
-        LEX_LUTHOR.delete()
+    if (Spiderman.isTouching(Maléfica)) {
+        Spiderman.delete()
+        Maléfica.delete()
         for (let index = 0; index < 3; index++) {
-            basic.showIcon(IconNames.Heart)
+            basic.showIcon(IconNames.Yes)
             basic.pause(100)
             basic.clearScreen()
             basic.pause(100)
         }
-        music.playMelody("C D E F G A B C5 ", 120)
+        music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
         game.gameOver()
     }
 })
